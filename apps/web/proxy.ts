b8424cfc5fd14cae44b8b7ddb0c6492cf5bdb2c1
Quang Vu <truongquangvu41@gmail.com>
2026-06-01 05:29:46 +0700

    refactor: add debug log to fe proxy

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,33 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { authClient } from "@/lib/auth/auth-client";
-import {
-  API_AUTH_PREFIX,
-  AUTH_ROUTES,
-  DEFAULT_LOGIN_REDIRECT,
-  PUBLIC_API_PREFIXES,
-} from "@/constants/routes";
+import { API_AUTH_PREFIX, AUTH_ROUTES, DEFAULT_LOGIN_REDIRECT, PUBLIC_API_PREFIXES } from "@/constants/routes";
 
 export async function proxy(req: NextRequest) {
   const { nextUrl } = req;
   const isApiAuthRoute = nextUrl.pathname.startsWith(API_AUTH_PREFIX);
-  const isPublicApiRoute = PUBLIC_API_PREFIXES.some((prefix) =>
-    nextUrl.pathname.startsWith(prefix),
-  );
+  const isPublicApiRoute = PUBLIC_API_PREFIXES.some((prefix) => nextUrl.pathname.startsWith(prefix));
 
   if (isApiAuthRoute || isPublicApiRoute) return NextResponse.next();
 
   const session = await authClient.getSession({
     fetchOptions: { headers: req.headers },
   });
-  const isAuthRoute = AUTH_ROUTES.some((route) =>
-    nextUrl.pathname.startsWith(route),
-  );
+  const isAuthRoute = AUTH_ROUTES.some((route) => nextUrl.pathname.startsWith(route));
+
+  console.log(session);
 
   if (isAuthRoute) {
-    if (session.data)
-      return NextResponse.redirect(
-        new URL(DEFAULT_LOGIN_REDIRECT, nextUrl.origin),
-      );
+    if (session.data) return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl.origin));
     return NextResponse.next();
   }
 
@@ -37,12 +27,7 @@ export async function proxy(req: NextRequest) {
       callBackUrl += nextUrl.search;
     }
     const encodedCallbackUrl = encodeURIComponent(callBackUrl);
-    return NextResponse.redirect(
-      new URL(
-        `/accounts/login?callbackURL=${encodedCallbackUrl}`,
-        nextUrl.origin,
-      ),
-    );
+    return NextResponse.redirect(new URL(`/accounts/login?callbackURL=${encodedCallbackUrl}`, nextUrl.origin));
   }
 
   return NextResponse.next();
